546100a952a0f5b7437c2c8e5d35d2e595c11899
Thomas Haysom <thomas.haysom@pencilwrench.com>
2023-12-07 18:07:38 -0500

    add sitemaps.txt and robots.txt

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets");
+    eleventyConfig.addPassthroughCopy("src/robots.txt");
+    eleventyConfig.addPassthroughCopy("src/sitemap.txt");
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
     return {
